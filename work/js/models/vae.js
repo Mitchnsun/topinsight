b40ca8a1dc8d27d4342d6570ee717b3e633ae4d5
matthieu_comperat <matthieu.comperat@gmail.com>
2017-01-04 10:57:14 +0100

    WS Vae - update callback

--- work/js/models/vae.js
+++ work/js/models/vae.js
@@ -2,18 +2,7 @@ define(['backbone'], function(Backbone) {
     return Backbone.Model.extend({
         url: app.urls.endpoint + app.urls.ws_vae,
         initialize: function(options) {
-            this.fetch({
-                access_token: options.access_token
-            }, {
-                success: _.bind(this.success, this),
-                error: _.bind(this.error, this)
-            });
-        },
-        success: function(model, response, options) {
-            console.log(model, response, options);
-        },
-        error: function(model, response, options) {
-            console.log(model, response, options);
+            this.fetch({ access_token: options.access_token });
         }
     });
 });
