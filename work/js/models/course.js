0cf65fa636e7d2e38f8f249263267b8b88ea86b8
matthieu_comperat <matthieu.comperat@gmail.com>
2017-01-08 16:32:25 +0100

    WS Course - Parse response

--- work/js/models/course.js
+++ work/js/models/course.js
@@ -1,5 +1,9 @@
 define(['backbone'], function(Backbone) {
     return Backbone.Model.extend({
-        url: app.urls.endpoint + app.urls.ws_course
+        url: app.urls.endpoint + app.urls.ws_course,
+        parse: function(response, options) {
+            console.log(response, options);
+            return response.course;
+        }
     });
 });
